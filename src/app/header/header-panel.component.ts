45d8a7f8f45ee342b531b42a9c19bcca2a84da4d
ermakovis <ermakovis@gmail.com>
2021-07-27 16:26:39 +0300

    header fix

--- src/app/header/header-panel.component.ts
+++ src/app/header/header-panel.component.ts
@@ -4,9 +4,7 @@ import { AuthService } from "../profile/auth.service";
 @Component({
     selector: "header-panel",
     templateUrl: "./header-panel.component.html",
-    styles: [`
-        .btn { padding: 0.1rem 0.6rem; }
-    `]
+    styleUrls: ["./header-panel.component.css"]
 })
 export class HeaderPanelComponent{
     authService : AuthService;
