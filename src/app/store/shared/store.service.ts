6563ced99a11183688741e64397f54f48897fcd1
ermakovis <ermakovis@gmail.com>
2021-07-27 15:58:10 +0300

    backend connection added

--- src/app/store/shared/store.service.ts
+++ src/app/store/shared/store.service.ts
@@ -1,57 +1,31 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable, Subject } from "rxjs";
+import { Observable, of, Subject } from "rxjs";
+import { catchError } from 'rxjs/operators'; 
 import { StoreItemModel } from "./store-item.model";
 
 @Injectable()
 export class StoreService {
+    constructor(private httpClient: HttpClient) {}
+
     getItems() : Observable<StoreItemModel[]> {
-        let subject = new Subject<StoreItemModel[]>();
-        setTimeout(() => {
-            subject.next(ITEMS);
-            subject.complete;
-        }, 100)
-        return subject;
+        return this.httpClient.get<StoreItemModel[]>('/zuul/service/get-products')
+                .pipe(catchError(this.handleError<StoreItemModel[]>('getEvents', [])))
+    }
+
+    private handleError<T> (operation = 'operation', result?: T) {
+        return (error: any) : Observable<T> => {
+            console.error(error)
+            return of(result as T)
+        }
     }
 
     //TODO Add error handler
     getItem(id:number) {
-        let item = ITEMS.find(item => item.id === id);
-        if (item === undefined) {
-            throw new TypeError('No such element');
+        return {
+            id: 1,
+            title: "qwe",
+            cost: 100
         }
-        return item;
     }
 }
-
-const ITEMS : StoreItemModel[] = [
-    {
-        id: 1,
-        title: 'Белая пиксельная коробка',
-        description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-        cost: 150
-    },
-    {
-        id: 2,
-        title: 'Белая пиксельная коробка',
-        description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-        cost: 150
-    },
-    {
-        id: 3,
-        title: 'Белая пиксельная коробка',
-        description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-        cost: 150
-    },
-    {
-        id: 4,
-        title: 'Белая пиксельная коробка',
-        description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-        cost: 150
-    },
-    {
-        id: 5,
-        title: 'Белая пиксельная коробка',
-        description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-        cost: 150
-    }
-]
